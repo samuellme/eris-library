d45f4a1c0be4f717c633e72e894bb83614da5fd2
samuellme <samuellme.ras@gmail.com>
2023-02-13 21:34:21 +0330

    feat(*): add basic source code
    just run Whitelabel Error Page on localhost:8080

---
+++ src/main/java/me/samuell/erislibrary/ErisLibraryApplication.java
@@ -0,0 +1,13 @@
+package me.samuell.erislibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErisLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErisLibraryApplication.class, args);
+	}
+
+}
